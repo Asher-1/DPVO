e5f4dedb3bd81245a889b118ff87c496eb001281
Dahai <Asher-1@users.noreply.github.com>
2024-08-07 23:24:12 +0800

    fix docker build issues

--- DPVO_Docker/install_dpvo.sh
+++ DPVO_Docker/install_dpvo.sh
@@ -10,5 +10,8 @@ make -j8
 sudo make install
 cd ../..
 
+./cmake-3.30.2-linux-x86_64.sh --skip-license --prefix=/usr/local
+update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
+
 pip install ./DPViewer
 ldconfig
